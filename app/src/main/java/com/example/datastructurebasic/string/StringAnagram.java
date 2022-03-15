43ab4ea968aeb1c2375f73f9ba94196290809231
Prerna <>
2022-03-15 12:40:25 +0530

    Initial set and dependency for Dragger2

--- app/src/main/java/com/example/datastructurebasic/string/StringAnagram.java
+++ app/src/main/java/com/example/datastructurebasic/string/StringAnagram.java
@@ -1,9 +1,11 @@
-package com.example.datastructurebasic.string;
+ package com.example.datastructurebasic.string;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by PrernaSurbhi on 13/03/22.
@@ -30,10 +32,19 @@ public class StringAnagram {
         }else{
             System.out.print("Anagram not found: ");
         }
+
+
+//        isAnagramUsingMap(str1,str2);
+//        if(isAnagramUsingMap(str1,str2)){
+//            System.out.print("Anagram found :");
+//        }else{
+//            System.out.print("Anagram not found: ");
+//        }
     }
 
     public static boolean isAnanagram(String str1, String str2){
-        if(str1.length() != str2.length()){
+        //base condition
+        if(str1 == null || str2 == null|| str1.length() != str2.length()){
             return false;
         }else{
             char[] ch1 = str1.toLowerCase().toCharArray();
@@ -44,5 +55,39 @@ public class StringAnagram {
 
             return Arrays.equals(ch1,ch2);
         }
+    }
+
+    public static boolean isAnagramUsingMap(String str1, String str2){
+        if(str1 == null || str2 == null|| str1.length() != str2.length()){
+            return false;
+        }
+
+        Map<Character,Integer> map = new HashMap<>();
+
+        //maintain count of each character of x on the map.
+        //getOrDefault - java 8 concept , if value present , then it other wise resturn default value.
+
+        for(char c:str1.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+
+        //do for each character of str2
+        //if the str2 character not present , that means that character is not in str1, so not a anagram
+        for(char c:str2.toCharArray()){
+            if(!map.containsKey(c)){
+                return false;
+            }
+
+            //decrease frequency of str2 on the map
+            map.put(c,map.get(c)-1);
+
+            //if the frequency become zero , eracse it form map
+
+            if(map.get(c) == 0){
+                map.remove(c);
+            }
+        }
+
+        return map.isEmpty();
     }
 }
